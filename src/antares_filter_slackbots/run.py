7befa6247e221f232e7187b910109bb7895b1561
kdesoto-psu <kmd6851@psu.edu>
2025-02-04 12:58:53 -0500

    fixed run.py

--- src/antares_filter_slackbots/run.py
+++ src/antares_filter_slackbots/run.py
@@ -5,24 +5,8 @@ from antares_filter_slackbots.filters import SuperphotPlusZTF, ShapleyPlotLAISS
 def all_current_filters():
     """Where all filters to run are defined.
     """
-    """
-    RankingFilter(
-        "LAISS_anomalies",
-        ShapleyPlotLAISS(),
-        "#anomaly-detection",
-        "LAISS_RFC_anomaly_score",
-        pre_filter_properties = {
-            "oldest_alert_observation_time": (current_time-1000., 99_999_999,),
-            "num_mag_values": (3, 500),
-            "LAISS_RFC_anomaly_score": (30., 100.),
-        },
-        pre_filter_tags = ["LAISS_RFC_AD_filter",],
-        save_properties = ["shap_url"]
-    ),
-    """
     current_time = Time.now().mjd
     all_filters = [
-        # anomaly detection filter: currently just uses ANTARES version of filter
         # superphot-plus filter
         RankingFilter(
             "superphot-plus-bright",
@@ -52,6 +36,20 @@ def all_current_filters():
             post_filter_tags = ["superphot_plus_classified",],
             post_filter_properties = {"superphot_plus_valid": (1,1), "superphot_plus_class_prob": (0.4, 1.0)},
             groupby_properties={'superphot_plus_class': ('SN IIn', 'SN Ibc')}
+        ),
+        # anomaly detection filter: currently just uses ANTARES version of filter
+        RankingFilter(
+            "LAISS_anomalies",
+            ShapleyPlotLAISS(),
+            "#anomaly-detection",
+            "LAISS_RFC_anomaly_score",
+            pre_filter_properties = {
+                "oldest_alert_observation_time": (current_time-1000., 99_999_999,),
+                "num_mag_values": (3, 500),
+                "LAISS_RFC_anomaly_score": (30., 100.),
+            },
+            pre_filter_tags = ["LAISS_RFC_AD_filter",],
+            save_properties = ["shap_url"]
         ),
     ]
     return all_filters
